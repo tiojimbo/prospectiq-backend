8824c5ea160744412c56a92885d55eca1ced139d
tiojimbo <isamuelmacedo0@gmail.com>
2025-06-07 23:40:44 -0300

    Corrige dependências para build no Render (Windows fix)

--- src/routes/clickup.ts
+++ src/routes/clickup.ts
@@ -1,8 +1,17 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+
 const router = Router();
 
-router.get("/auth", (req, res) => {
+router.get("/auth", (req: Request, res: Response) => {
   res.send("Rota de autenticação ClickUp funcionando!");
+});
+
+router.get("/callback", async (req: Request, res: Response) => {
+  res.send("Rota de callback ClickUp funcionando!");
+});
+
+router.post("/create-task", async (req: Request, res: Response) => {
+  res.send("Criação de tarefa ClickUp funcionando!");
 });
 
 export default router;
